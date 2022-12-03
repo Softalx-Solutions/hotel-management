6e24336c902b2b8a78c15749f955edcd4a8bf743
Edafe Oke Great <greatedafeoke@gmail.com>
2022-12-03 23:53:44 +0100

    create header component

--- frontend/web/admin/src/components/NavBar.jsx
+++ frontend/web/admin/src/components/NavBar.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import logo from "../assets/logo.svg";
 import overviewLogo from "../assets/overview.svg";
+import { links } from "../data";
+
 function NavBar() {
   return (
     <nav className="col-span-1 bg-dark">
@@ -11,23 +13,15 @@ function NavBar() {
         </h1>
       </div>
       <ul className="mt-8">
+        {links.map((link) => {
+          return (
+            <li className="flex p-3">
+              {link.icon}
+              <h3 className="text-[#DDE2FF] ml-4">{link.title}</h3>
+            </li>
+          );
+        })}
         <li className="flex p-3 bg-[#84868f] border-[#DDE2FF] border-l-4">
-            <img src={overviewLogo} alt="overview"/>
-            <h3 className="text-[#DDE2FF] ml-4">Overview</h3>
-        </li>
-        <li className="flex p-3 border-[#DDE2FF]">
-            <img src={overviewLogo} alt="overview"/>
-            <h3 className="text-[#DDE2FF] ml-4">Overview</h3>
-        </li>
-        <li className="flex p-3 border-[#DDE2FF]">
-            <img src={overviewLogo} alt="overview"/>
-            <h3 className="text-[#DDE2FF] ml-4">Overview</h3>
-        </li>
-        <li className="flex p-3 border-[#DDE2FF]">
-            <img src={overviewLogo} alt="overview"/>
-            <h3 className="text-[#DDE2FF] ml-4">Overview</h3>
-        </li>
-        <li className="flex p-3 border-[#DDE2FF]">
             <img src={overviewLogo} alt="overview"/>
             <h3 className="text-[#DDE2FF] ml-4">Overview</h3>
         </li>
