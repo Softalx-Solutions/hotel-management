e62dd2130fdf721aa3f5038b26acb78f8825eacb
alexohre <alexokomado@gmail.com>
2022-12-29 12:55:35 +0100

    added print functionality with react-to-print libary
    Signed-off-by: alexohre <alexokomado@gmail.com>

--- frontend/web/hotel-reception/src/partials/orders/InvioceCard.jsx
+++ frontend/web/hotel-reception/src/partials/orders/InvioceCard.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
 
-function InvioceCard() {
+const InvioceCard = React.forwardRef((props, ref)=> {
 
-  const handlePrint =(e)=>{
-    e.preventDefault();
-    window.print();
-  }
+
   return (
-    <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
+    <div ref={ref} className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto" >
 
-              <div className="max-w-2xl m-auto my-16">
+              <div className="max-w-2xl m-auto my-16" >
 
                 <div className="text-center bg-white shadow-md rounded border border-slate-200 p-10">
                 {/* <InvioceCard /> */}
@@ -21,7 +18,7 @@ function InvioceCard() {
                     </svg>
                   </div>
                   
-                  <h2 className="text-2xl text-slate-800 font-bold mb-2">OneSpring Hotel & Suite</h2>
+                  <h2 className="text-2xl text-rose-500 font-bold mb-2">OneSpring Hotel & Suite</h2>
                   <div className="">756 Java Lane</div>
                   <div className="">Columbia, SC 29210 .</div>
                   <div className="mb-6">Rego Park Queens, NY 11734.</div>
@@ -99,17 +96,11 @@ function InvioceCard() {
                   </div>
                   
                   
-                  <button onClick={handlePrint} className="btn bg-indigo-500 hover:bg-indigo-600 text-white mt-10">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-printer" viewBox="0 0 16 16">
-                      <path d="M2.5 8a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z"/>
-                      <path d="M5 1a2 2 0 0 0-2 2v2H2a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h1v1a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-1h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-1V3a2 2 0 0 0-2-2H5zM4 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2H4V3zm1 5a2 2 0 0 0-2 2v1H2a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1v-1a2 2 0 0 0-2-2H5zm7 2v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1z"/>
-                    </svg>
-                    <span className="ml-2">Print Reciept </span>
-                  </button>
+                  
                 </div>
               </div>
           </div>
   )
-}
+})
 
 export default InvioceCard
