f804234815936d721b05c8736d098d3b08456d82
Edafe Oke Great <greatedafeoke@gmail.com>
2023-01-18 13:38:18 +0100

    Pushing now hahahahah

--- frontend/web/hotel-admin/src/App.jsx
+++ frontend/web/hotel-admin/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
-
+import { Provider } from "react-redux";
 import "./css/style.css";
 
 import "./charts/ChartjsConfig";
@@ -46,7 +46,8 @@ import AvatarPage from "./pages/component/AvatarPage";
 import TooltipPage from "./pages/component/TooltipPage";
 import AccordionPage from "./pages/component/AccordionPage";
 import IconsPage from "./pages/component/IconsPage";
-import Reservations from "./pages/component/Reservations";
+// import Reservations from "./pages/component/Reservations";
+import {store} from './app/store'
 
 function App() {
   const location = useLocation();
@@ -59,51 +60,53 @@ function App() {
 
   return (
     <>
-      <Routes>
-        <Route exact path="/" element={<Dashboard />} />
-        <Route path="/analytics" element={<Analytics />} />
-        <Route path="/campaigns" element={<Campaigns />} />
-        <Route path="/messages" element={<Messages />} />
-        <Route path="/inbox" element={<Inbox />} />
-        <Route path="/calendar" element={<Calendar />} />
-        <Route path="/settings" element={<Account />} />
-        <Route path="/settings/account" element={<Account />} />
-        <Route path="/settings/notifications" element={<Notifications />} />
-        <Route path="/settings/apps" element={<Apps />} />
-        <Route path="/settings/plans" element={<Plans />} />
-        <Route path="/settings/billing" element={<Billing />} />
-        <Route path="/settings/feedback" element={<Feedback />} />
-        <Route path="/utility/changelog" element={<Changelog />} />
-        <Route path="/utility/roadmap" element={<Roadmap />} />
-        <Route path="/utility/faqs" element={<Faqs />} />
-        <Route path="/utility/empty-state" element={<EmptyState />} />
-        <Route path="/staff" element={<Staff />} />
-        <Route path="/rooms" element={<Rooms />} />
-        <Route path="/reservations" element={<Reservations />} />
-        <Route path="/utility/404" element={<PageNotFound />} />
-        <Route path="/utility/knowledge-base" element={<KnowledgeBase />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
-        <Route path="/onboarding-01" element={<Onboarding01 />} />
-        <Route path="/onboarding-02" element={<Onboarding02 />} />
-        <Route path="/onboarding-03" element={<Onboarding03 />} />
-        <Route path="/onboarding-04" element={<Onboarding04 />} />
-        <Route path="/component/button" element={<ButtonPage />} />
-        <Route path="/component/form" element={<FormPage />} />
-        <Route path="/component/dropdown" element={<DropdownPage />} />
-        <Route path="/component/alert" element={<AlertPage />} />
-        <Route path="/component/modal" element={<ModalPage />} />
-        <Route path="/component/pagination" element={<PaginationPage />} />
-        <Route path="/component/tabs" element={<TabsPage />} />
-        <Route path="/component/breadcrumb" element={<BreadcrumbPage />} />
-        <Route path="/component/badge" element={<BadgePage />} />
-        <Route path="/component/avatar" element={<AvatarPage />} />
-        <Route path="/component/tooltip" element={<TooltipPage />} />
-        <Route path="/component/accordion" element={<AccordionPage />} />
-        <Route path="/component/icons" element={<IconsPage />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
+      <Provider store={store}>
+        <Routes>
+          <Route exact path="/" element={<Dashboard />} />
+          <Route path="/analytics" element={<Analytics />} />
+          <Route path="/campaigns" element={<Campaigns />} />
+          <Route path="/messages" element={<Messages />} />
+          <Route path="/inbox" element={<Inbox />} />
+          <Route path="/calendar" element={<Calendar />} />
+          <Route path="/settings" element={<Account />} />
+          <Route path="/settings/account" element={<Account />} />
+          <Route path="/settings/notifications" element={<Notifications />} />
+          <Route path="/settings/apps" element={<Apps />} />
+          <Route path="/settings/plans" element={<Plans />} />
+          <Route path="/settings/billing" element={<Billing />} />
+          <Route path="/settings/feedback" element={<Feedback />} />
+          <Route path="/utility/changelog" element={<Changelog />} />
+          <Route path="/utility/roadmap" element={<Roadmap />} />
+          <Route path="/utility/faqs" element={<Faqs />} />
+          <Route path="/utility/empty-state" element={<EmptyState />} />
+          <Route path="/staff" element={<Staff />} />
+          <Route path="/rooms" element={<Rooms />} />
+          {/* <Route path="/reservations" element={<Reservations />} /> */}
+          <Route path="/utility/404" element={<PageNotFound />} />
+          <Route path="/utility/knowledge-base" element={<KnowledgeBase />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+          <Route path="/onboarding-01" element={<Onboarding01 />} />
+          <Route path="/onboarding-02" element={<Onboarding02 />} />
+          <Route path="/onboarding-03" element={<Onboarding03 />} />
+          <Route path="/onboarding-04" element={<Onboarding04 />} />
+          <Route path="/component/button" element={<ButtonPage />} />
+          <Route path="/component/form" element={<FormPage />} />
+          <Route path="/component/dropdown" element={<DropdownPage />} />
+          <Route path="/component/alert" element={<AlertPage />} />
+          <Route path="/component/modal" element={<ModalPage />} />
+          <Route path="/component/pagination" element={<PaginationPage />} />
+          <Route path="/component/tabs" element={<TabsPage />} />
+          <Route path="/component/breadcrumb" element={<BreadcrumbPage />} />
+          <Route path="/component/badge" element={<BadgePage />} />
+          <Route path="/component/avatar" element={<AvatarPage />} />
+          <Route path="/component/tooltip" element={<TooltipPage />} />
+          <Route path="/component/accordion" element={<AccordionPage />} />
+          <Route path="/component/icons" element={<IconsPage />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Provider>
     </>
   );
 }
